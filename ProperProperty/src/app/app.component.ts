7a37abcf00d967643fd4ccb103712f6bf3a6ddb1
Thapelo64 <68476683+Thapelo64@users.noreply.github.com>
2023-05-16 06:41:16 +0200

    eslint

--- ProperProperty/src/app/app.component.ts
+++ ProperProperty/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   constructor() {}
 }
