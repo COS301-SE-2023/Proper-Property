0e391e3b4b29a892460ea7679d0cdd5575ab0d79
Thapelo64 <68476683+Thapelo64@users.noreply.github.com>
2023-05-17 16:51:52 +0200

    listings page implemented

--- proper-property/src/app/listings/listings.module.ts
+++ proper-property/src/app/listings/listings.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { ListingsPage } from './listings.page';
 
 import { ListingsPageRoutingModule } from './listings-routing.module';
 
-import { ListingsPage } from './listings.page';
 
 @NgModule({
   imports: [
