7bdca39675c681e7116a8173e34272d0ad378eb3
Thapelo64 <68476683+Thapelo64@users.noreply.github.com>
2023-05-17 17:43:35 +0200

    security measure

--- proper-property/src/environments/environment.prod.ts
+++ proper-property/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  googleMapsApiKey: 'AIzaSyCI8fuLmmnPFtqvfVHzT6hxu5oa-Y6TrRg'
+  googleMapsApiKey: ''
 };
--- proper-property/src/environments/environment.ts
+++ proper-property/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  googleMapsApiKey: 'AIzaSyCI8fuLmmnPFtqvfVHzT6hxu5oa-Y6TrRg'
+  googleMapsApiKey: ''
 };
 
 /*
