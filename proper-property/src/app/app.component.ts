cc8a62a75e754b972a42e52ce84c674e700c2762
Thapelo64 <68476683+Thapelo64@users.noreply.github.com>
2023-05-18 09:50:33 +0200

    individual listing image view

--- proper-property/src/app/app.component.ts
+++ proper-property/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {register} from 'swiper/element/bundle';
+
+register();
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
