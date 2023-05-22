a74f079c59b18ef7e86503acb824f1e76c0d2e84
Rob-0ff <robofficer26@gmail.com>
2023-05-22 16:11:19 +0200

    added the auth service files

---
+++ src/app/services/auth/auth.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { GoogleAuthProvider } from 'firebase/auth';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(
+    public afAuth: AngularFireAuth // Inject Firebase auth service
+  ) {}
+  // Sign in with Google
+  GoogleAuth() {
+    return this.AuthLogin(new GoogleAuthProvider());
+  }
+  // Auth logic to run auth providers
+  AuthLogin(provider : any) {
+    return this.afAuth
+      .signInWithPopup(provider)
+      .then((result) => {
+        console.log('You have been successfully logged in!');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+}
