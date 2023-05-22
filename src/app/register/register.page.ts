d572647904df7c1b507b21989007628326efb210
Rob-0ff <robofficer26@gmail.com>
2023-05-22 21:19:35 +0200

    Routing for register page
    Co-Authored-By: Wian Koekemoer <105972926+slaaiblaar@users.noreply.github.com>

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth/auth.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -8,7 +9,7 @@ import { AuthService } from '../services/auth/auth.service';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-  constructor(public authService: AuthService) {
+  constructor(public authService: AuthService, public router: Router) {
     this.name = this.surname = this.password = this.email = this.confirm_password = "";
   }
 
@@ -24,7 +25,11 @@ export class RegisterPage implements OnInit {
       this.passwordMatch = false;
       return; // Prevent further execution
     }
-    this.authService.register(this.email, this.password);
+    this.authService.register(this.email, this.password).then((res) => {
+      if(res !== null){
+        this.router.navigate(['/home']);
+      }
+    });
   }
 
   checkPassword() {
