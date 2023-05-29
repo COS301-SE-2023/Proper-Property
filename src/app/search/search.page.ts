971afbeec1549134ce8e1f49deac4a0914b9602f
Thapelo64 <68476683+Thapelo64@users.noreply.github.com>
2023-05-29 10:59:24 +0200

    Buying and renting tabs

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -1,15 +1,66 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Property {
+  title: string;
+  type: string;
+  price: number;
+  bedrooms: number;
+}
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.page.html',
   styleUrls: ['./search.page.scss'],
 })
-export class SearchPage implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
+export class SearchPage {
+  activeTab: string = 'buying';
+  searchQuery: string = '';
+  selectedPropertyType: string = '';
+  selectedMinPrice: number = 0;
+  selectedMaxPrice: number = 0;
+  selectedBedrooms: number = 0;
+  properties: Property[] = [
+    { title: 'House 1', type: 'house', price: 100000, bedrooms: 3 },
+    { title: 'Apartment 1', type: 'apartment', price: 1500, bedrooms: 2 },
+    { title: 'Condo 1', type: 'condo', price: 2000, bedrooms: 1 },
+    // Add more properties here
+  ];
+
+  get filteredBuyingProperties(): Property[] {
+    return this.properties.filter(property =>
+      property.type.includes(this.selectedPropertyType) &&
+      property.price >= this.selectedMinPrice &&
+      property.price <= this.selectedMaxPrice &&
+      (this.selectedBedrooms === 0 || property.bedrooms === this.selectedBedrooms) &&
+      property.title.toLowerCase().includes(this.searchQuery.toLowerCase())
+    );
+  }
+
+  get filteredListingProperties(): Property[] {
+    // Add your own logic for filtering listing properties
+    // based on the selected filters and search query
+    return [];
+  }
+
+  filterProperties(): void {
+    // Update the filtered properties based on the selected filters and search query
+    if (this.activeTab === 'buying') {
+      this.filteredBuyingProperties;
+    } else if (this.activeTab === 'listing') {
+      this.filteredListingProperties;
+    }
+  }
+
+  changeTab(): void {
+    // Reset the selected filters and search query when changing tabs
+    this.selectedPropertyType = '';
+    this.selectedMinPrice = 0;
+    this.selectedMaxPrice = 0;
+    this.selectedBedrooms = 0;
+    this.searchQuery = '';
+    this.filterProperties();
   }
 
 }
