2045a26d0d103217a11e0f7b4ea07ab1e1828139
Rob-0ff <robofficer26@gmail.com>
2023-05-31 14:13:24 +0200

    Added grid for adding photos and displays added photos

--- src/app/create-listing/create-listing.page.ts
+++ src/app/create-listing/create-listing.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { signOut } from 'firebase/auth';
 
 @Component({
   selector: 'app-create-listing',
@@ -12,4 +13,40 @@ export class CreateListingPage implements OnInit {
   ngOnInit() {
   }
 
+  photos: string[] = [];
+  count = 0;
+
+  handleFileInput(event: any) {
+    const files = event.target.files;
+    if (files && files.length) {
+      for (const file of files) {
+        this.photos.push(URL.createObjectURL(file));
+        const currentImages = document.getElementById('current-images');
+        this.count++;
+
+        if (currentImages){
+          currentImages.innerHTML += "<div style='width: 10%; height: 100%;'><img class='single-image' src='" + this.photos[this.photos.length - 1] + "'></img><ion-icon name='close-circle' (click)='removeImage(" + this.count + ")'</ion-icon></div>&nbsp;";
+          console.log(this.photos[this.photos.length - 1]);
+          console.log(currentImages.innerHTML);
+        }
+      }
+    }
+  }
+
+  removeImage(index: number) {
+    this.photos.splice(index, 1);
+    console.log(this.photos);
+  }
+
+  selectPhotos() {
+    const fileInput = document.querySelector('input[type="file"]');
+    if (fileInput) {
+      const event = new MouseEvent('click', {
+        view: window,
+        bubbles: false,
+        cancelable: true
+      });
+      fileInput.dispatchEvent(event);
+    }
+  }
 }
