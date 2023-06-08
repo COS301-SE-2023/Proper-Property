52b25797e54c243943fec2e7d40396db7362851f
Rob-0ff <robofficer26@gmail.com>
2023-06-08 22:36:38 +0200

    - Began creating a basic outline for how the add lisitngs page will look - Began adding current user states to the application but not working

---
+++ src/app/profile/interfaces/profile.interface.ts
@@ -0,0 +1,6 @@
+export interface profile{
+    user_id?: string;
+    first_name: string;
+    last_name: string;
+    email: string;
+}
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth/auth.service';
 import { Router } from '@angular/router';
-
+import { profile } from '../profile/interfaces/profile.interface';
+import { UserService } from '../services/user/user.service';
 
 @Component({
   selector: 'app-register',
@@ -9,7 +10,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-  constructor(public authService: AuthService, public router: Router) {
+  constructor(public authService: AuthService, public router: Router, public userService: UserService) {
     this.name = this.surname = this.password = this.email = this.confirm_password = "";
   }
 
@@ -26,6 +27,12 @@ export class RegisterPage implements OnInit {
       return; // Prevent further execution
     }
     this.authService.register(this.email, this.password).then((res) => {
+      let user : profile = {
+        email: this.email,
+        first_name: this.name,
+        last_name: this.surname,
+      }
+      this.userService.registerNewUser(user);
       if(res !== null){
         this.router.navigate(['/home']);
       }
