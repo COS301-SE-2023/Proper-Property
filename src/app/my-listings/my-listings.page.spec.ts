334d1f51b036537d13ddbca38581cdcb0b62b523
Thapelo64 <68476683+Thapelo64@users.noreply.github.com>
2023-06-23 09:51:03 +0200

    Update my-listings.page.spec.ts

--- src/app/my-listings/my-listings.page.spec.ts
+++ src/app/my-listings/my-listings.page.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { MyListingsPage } from './my-listings.page';
 
 describe('MyListingsPage', () => {
