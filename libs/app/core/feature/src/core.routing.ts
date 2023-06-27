90307959e1773039593d5d8ff7a1088f00279038
slaaiblaar <u19043512@tuks.co.za>
2023-06-27 17:16:50 +0200

    routing was ignored for some reason

---
+++ libs/app/core/feature/src/core.routing.ts
@@ -0,0 +1,81 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('@properproperty/app/home/feature').then( m => m.HomePageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('@properproperty/app/login/feature').then( m => m.LoginPageModule)
+  },
+  // {
+  //   path: 'register',
+  //   loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  // },
+  // {
+  //   path: 'profile',
+  //   loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
+  // },
+  // {
+  //   path: 'search',
+  //   loadChildren: () => import('./search/search.module').then( m => m.SearchPageModule)
+  // },
+  // {
+  //   path: 'loading',
+  //   loadChildren: () => import('./loading/loading.module').then( m => m.LoadingPageModule)
+  // },
+  // {
+  //   path: 'listings',
+  //   loadChildren: () => import('./listings/listings.module').then( m => m.ListingsPageModule)
+  // },
+  // {
+  //   path: 'copyright',
+  //   loadChildren: () => import('./copyright/copyright.module').then( m => m.CopyrightPageModule)
+  // },
+  // {
+  //   path: 'version',
+  //   loadChildren: () => import('./version/version.module').then( m => m.VersionPageModule)
+  // },
+  // {
+  //   path: 'create-listing',
+  //   loadChildren: () => import('./create-listing/create-listing.module').then( m => m.CreateListingPageModule)
+  // },
+  // {
+  //   path: 'settings',
+  //   loadChildren: () => import('./settings/settings.module').then( m => m.SettingsPageModule)
+  // },
+  // {
+  //   path: 'listing',
+  //   loadChildren: () => import('./listing/listing.module').then( m => m.ListingPageModule)
+  // },
+  // {
+  //   path: 'profile',
+  //   loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
+  // },
+  // {
+  //   path: 'saved-listings',
+  //   loadChildren: () => import('./saved-listings/saved-listings.module').then( m => m.SavedListingsPageModule)
+  // },
+  // {
+  //   path: 'my-listings',
+  //   loadChildren: () => import('./my-listings/my-listings.module').then( m => m.MyListingsPageModule)
+  // }
+
+
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule],
+  
+})
+export class CoreRouting {}
