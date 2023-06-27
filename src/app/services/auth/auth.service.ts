cb0ea956356837f4a1b475e2ebf75bd8749886c5
Thapelo64 <68476683+Thapelo64@users.noreply.github.com>
2023-06-27 14:44:37 +0200

    auth cookies

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -98,4 +98,56 @@ export class AuthService {
   }
   
 
+  private readonly AUTH_COOKIE_NAME = 'authToken';
+  private readonly PROFILE_COOKIE_NAME = 'profileData';
+
+
+  // Save authentication token in a cookie
+  saveAuthToken(token: string) {
+    setCookie(this.AUTH_COOKIE_NAME, token, 7); // Set the cookie to expire in 7 days
+  }
+
+  // Get authentication token from the cookie
+  getAuthToken() {
+    return getCookie(this.AUTH_COOKIE_NAME);
+  }
+
+  // Save profile data in a cookie
+  saveProfileData(profile: any) {
+    setCookie(this.PROFILE_COOKIE_NAME, JSON.stringify(profile), 7); // Set the cookie to expire in 7 days
+  }
+
+  // Get profile data from the cookie
+  getProfileData() {
+    const profileCookie = getCookie(this.PROFILE_COOKIE_NAME);
+    return profileCookie ? JSON.parse(profileCookie) : null;
+  }
+
+  // Clear the authentication and profile cookies
+  clearCookies() {
+    document.cookie = this.AUTH_COOKIE_NAME + "=; expires=Thu, 01 Jan 2025 00:00:00 UTC; path=/;";
+    document.cookie = this.PROFILE_COOKIE_NAME + "=; expires=Thu, 01 Jan 2025 00:00:00 UTC; path=/;";
+  }
+  
+}
+
+function getCookie(name: string) {
+  const cookieName = name + "=";
+  const cookieArray = document.cookie.split(';');
+  for (let i = 0; i < cookieArray.length; i++) {
+    let cookie = cookieArray[i];
+    while (cookie.charAt(0) === ' ') {
+      cookie = cookie.substring(1);
+    }
+    if (cookie.indexOf(cookieName) === 0) {
+      return decodeURIComponent(cookie.substring(cookieName.length));
+    }
+  }
+  return "";
+}
+function setCookie(name: string, value: string, days: number) {
+  const expirationDate = new Date();
+  expirationDate.setDate(expirationDate.getDate() + days);
+  const cookieValue = encodeURIComponent(value) + "; expires=" + expirationDate.toUTCString() + "; path=/";
+  document.cookie = name + "=" + cookieValue;
 }
