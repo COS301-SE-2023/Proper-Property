1a8a49df56f06a16239bb4a44f4c07d7edd0e919
Thapelo64 <68476683+Thapelo64@users.noreply.github.com>
2023-06-30 11:15:43 +0200

    Integrated search by Property Type,Min Price, Max price, bedrooms, bathrooms, erf and floor size

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { GmapsService } from '../services/gmaps.service';
+import { Component, ElementRef, OnDestroy, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { ActionSheetController, ModalController } from '@ionic/angular';
+import { ListingsService } from '../services/listings/listings.service';
+import { Router } from '@angular/router';
+import { listing } from '../listing/interfaces/listing.interface';
 
 interface Property {
   title: string;
@@ -14,7 +19,173 @@ interface Property {
 })
 
 
-export class SearchPage {
+export class SearchPage implements OnDestroy, OnInit {
+  @ViewChild('map', { static: true })
+  mapElementRef!: ElementRef;
+  googleMaps: any;
+  center = { lat: -25.7477, lng: 28.2433 };
+  map: any;
+  mapClickListener: any;
+  markerClickListener: any;
+  markers: any[] = [];
+  listings: listing[] = []
+
+
+  constructor(
+    private gmaps: GmapsService,
+    private renderer: Renderer2,
+    private actionSheetCtrl: ActionSheetController,
+    private router: Router,
+    private listingServices : ListingsService,
+    ) {}
+
+  async ngOnInit() {
+    await this.listingServices.getListings().then((listings) => {
+      this.listings = listings;
+    });
+  }
+
+  ngAfterViewInit() {
+    this.loadMap();
+  }
+
+  async loadMap() {
+    try {
+      let googleMaps: any = await this.gmaps.loadGoogleMaps();
+      this.googleMaps = googleMaps;
+      const mapEl = this.mapElementRef.nativeElement;
+      const location = new googleMaps.LatLng(this.center.lat, this.center.lng);
+      this.map = new googleMaps.Map(mapEl, {
+        center: location,
+        zoom: 12,
+      });
+      this.renderer.addClass(mapEl, 'visible');
+      this.addMarker(location);
+      this.onMapClick();
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+  onMapClick() {
+    this.mapClickListener = this.googleMaps.event.addListener(this.map, "click", (mapsMouseEvent: { latLng: { toJSON: () => any; }; }) => {
+      console.log(mapsMouseEvent.latLng.toJSON());
+      this.addMarker(mapsMouseEvent.latLng);
+    });
+  }
+
+  addMarker(location: any) {
+    let googleMaps: any = this.googleMaps;
+    const icon = {
+      url: 'assets/icon/map_card.png',
+      scaledSize: new googleMaps.Size(100, 50), 
+    };
+    const marker = new googleMaps.Marker({
+      position: location,
+      map: this.map,
+      icon: icon,
+      // draggable: true,
+      animation: googleMaps.Animation.DROP
+    });
+    this.markers.push(marker);
+    // this.presentActionSheet();
+    this.markerClickListener = this.googleMaps.event.addListener(marker, 'click', () => {
+      console.log('markerclick', marker);
+      this.checkAndRemoveMarker(marker);
+      console.log('markers: ', this.markers);
+    });
+  }
+
+  checkAndRemoveMarker(marker: { position: { lat: () => any; lng: () => any; }; }) {
+    const index = this.markers.findIndex(x => x.position.lat() == marker.position.lat() && x.position.lng() == marker.position.lng());
+    console.log('is marker already: ', index);
+    if(index >= 0) {
+      this.markers[index].setMap(null);
+      this.markers.splice(index, 1);
+      return;
+    }
+  }
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Added Marker',
+      subHeader: '',
+      buttons: [
+        {
+          text: 'Remove',
+          role: 'destructive',
+          data: {
+            action: 'delete',
+          },
+        },
+        {
+          text: 'Save',
+          data: {
+            action: 'share',
+          },
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          data: {
+            action: 'cancel',
+          },
+        },
+      ],
+    });
+
+    await actionSheet.present();
+  }
+
+  async redirectToPage(listing : listing) {
+    console.log(listing.listing_id);
+    this.router.navigate(['/listing', {list : listing.listing_id}]);
+  }
+
+  ngOnDestroy() {
+    // this.googleMaps.event.removeAllListeners();
+    if(this.mapClickListener) this.googleMaps.event.removeListener(this.mapClickListener);
+    if(this.markerClickListener) this.googleMaps.event.removeListener(this.markerClickListener);
+  }
+  //likes:
+  isRed: boolean = false;
+
+  toggleColor() {
+    this.isRed = !this.isRed;
+  }
+
+  Templistings: listing[] = []
+
+  searchProperties(){
+    const filteredListings = [];
+
+    for (let i = 0; i < this.listings.length; i++) {
+      const listing = this.listings[i];
+  
+      //if (listing.address.toLowerCase().includes(this.searchQuery.toLowerCase())) 
+      {
+        if (listing.bed === this.selectedBedrooms.toString()) {
+          if (parseInt(listing.floor_size) >= this.selectedFloorSize) {
+            if (parseInt(listing.price) >= this.selectedMinPrice && parseFloat(listing.price) <= this.selectedMaxPrice) {
+              if (listing.prop_type.includes(this.selectedPropertyType)) {
+                //if (listing.furnish_type === this.selectedFurnishType)
+                 {
+                  //if (listing.property_size === this.selectedPropertySize) 
+                  {
+                    filteredListings.push(listing);
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    this.listings = filteredListings;
+
+  }
+
   activeTab: string = 'buying';
   searchQuery: string = '';
   selectedPropertyType: string = '';
@@ -54,7 +225,7 @@ export class SearchPage {
     );
   }
 
-  get filteredListingProperties(): Property[] {
+  get filteredRentingProperties(): Property[] {
     // Add your own logic for filtering listing properties
     // based on the selected filters and search query
     return [];
@@ -64,8 +235,8 @@ export class SearchPage {
     // Update the filtered properties based on the selected filters and search query
     if (this.activeTab === 'buying') {
       this.filteredBuyingProperties;
-    } else if (this.activeTab === 'listing') {
-      this.filteredListingProperties;
+    } else if (this.activeTab === 'rent') {
+      this.filteredRentingProperties;
     }
   }
 
