f5233e808bff2438229c8f830a8780847c7895f9
slaaiblaar <u19043512@tuks.co.za>
2023-07-03 16:09:35 +0200

    Tried using github secrets to instantiate env file during workflow

--- libs/app/core/feature/src/core.module.ts
+++ libs/app/core/feature/src/core.module.ts
@@ -63,7 +63,7 @@ import {
 import { API_KEY_TOKEN } from '@properproperty/app/google-maps/util';
 
 const NX_ENVIRONMENT = process.env['NX_ENVIRONMENT'] || 'development';
-const USE_EMULATORS = JSON.parse(process.env['USE_EMULATORS'] || 'true');
+const USE_EMULATORS = JSON.parse(process.env['NX_USE_EMULATORS'] || 'true');
 const NX_FIREBASE_CONFIG = {
   apiKey: process.env['NX_FIREBASE_KEY'],
   authDomain: process.env['NX_FIREBASE_AUTH_DOMAIN'],
