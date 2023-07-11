14c582ea7014ab973c83221910bf42c16e1283d3
Rob-0ff <robofficer26@gmail.com>
2023-07-11 21:21:03 +0200

    Attempting to add videos as artifacts to download the videos

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   e2e: {
     defaultCommandTimeout: 420000,
+    videoUploadOnPasses: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
