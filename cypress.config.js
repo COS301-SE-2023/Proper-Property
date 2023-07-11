dd4b5f73cebbdfcb268433290d9a576aa9695b7c
Keabetswe Madumo <98401898+MadKea@users.noreply.github.com>
2023-07-11 12:27:39 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   e2e: {
+
+    defaultCommandTimeout: 10000,
+    
     setupNodeEvents(on, config) {
       // implement node event listeners here
       defaultCommandTimeout: 10000
