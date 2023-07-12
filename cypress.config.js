67183f487720a9143b377b9e67456ae73866c5ab
Rob-0ff <robofficer26@gmail.com>
2023-07-12 16:08:31 +0200

    We try things

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   e2e: {
-    // defaultCommandTimeout: 420000,
+    defaultCommandTimeout: 8000,
     videoUploadOnPasses: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
