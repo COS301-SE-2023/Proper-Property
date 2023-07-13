c13b8aa124063e41ba189cd9a5563ba6d5eaf927
Keabetswe Madumo <98401898+MadKea@users.noreply.github.com>
2023-07-13 11:46:59 +0200

    k6 added

--- k6Loadtest.js
+++ k6Loadtest.js
@@ -1,9 +1,12 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { sleep, check } from 'k6';
 
 export const options = {
-  duration: '1m',
-  vus: 50,
+  duration: '5m',
+  vus: 5,
+  thresholds: {
+    http_req_duration: ['p(95)<500']
+  },
 };
 
 export default function () {
