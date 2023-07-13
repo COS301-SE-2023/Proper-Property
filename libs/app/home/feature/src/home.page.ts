675d3e97cfd5f2e6dc634d0138a4bffe33aaa6c8
Thapelo64 <68476683+Thapelo64@users.noreply.github.com>
2023-07-13 15:19:49 +0200

    Search Bar is implemented

--- libs/app/home/feature/src/home.page.ts
+++ libs/app/home/feature/src/home.page.ts
@@ -2,6 +2,7 @@ import { Component, inject, OnInit,ViewChild,ElementRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UserService } from '@properproperty/app/user/data-access';
 import { profile } from '@properproperty/app/profile/util';
+import { GmapsService } from '@properproperty/app/google-maps/data-access';
 import Swiper from 'swiper';
 // import { Storage, ref } from '@angular/fire/storage';
 // import { uploadBytes } from 'firebase/storage';
@@ -13,6 +14,13 @@ import Swiper from 'swiper';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  @ViewChild('address', { static: true }) addressInput!: ElementRef<HTMLInputElement>;
+
+  autocomplete: any;
+
+  predictions: google.maps.places.AutocompletePrediction[] = [];
+
+
   @ViewChild('swiper')
   swiperRef: ElementRef | undefined;
   swiper?: Swiper;
@@ -20,11 +28,14 @@ export class HomePage implements OnInit {
   swiperSlideChanged(e:Event) {
     console.log('changed', e)
   }
+
   
   public home!: string;
   private activatedRoute = inject(ActivatedRoute);
   currentUser: profile | null = null;
-  constructor(public userService : UserService) {
+  constructor(
+    public userService : UserService,
+    public gmapsService: GmapsService) {
     this.currentUser = this.userService.getCurrentUser();
   }
 
@@ -46,8 +57,19 @@ export class HomePage implements OnInit {
         signupBut.style.visibility = 'hidden';
       }
     }
+    
+    const inputElementId = 'address';
+
+    
+    
+    this.gmapsService.setupRegionSearchBox(inputElementId);
   }
 
+  searchQuery = '';
+  //to be implemented
+  searchProperties() {
+    this.searchQuery = (document.getElementById("address") as HTMLInputElement).value;
+  }
   // swiperReady() {
   //   this.swiper = this.swiperRef?.nativeElement.swiper;
   // }
