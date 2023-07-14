bcb5d6075124b57337fce11d8ba775170a396d63
Rob-0ff <robofficer26@gmail.com>
2023-07-14 12:46:38 +0200

    Added the admin page component

---
+++ libs/app/admin/feature/src/lib/admin.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminPage } from './admin.page';
+import { IonicModule } from '@ionic/angular';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    IonicModule
+  ],
+  declarations: [AdminPage],
+})
+export class AdminPageModule {}
---
+++ libs/app/admin/feature/src/lib/admin.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'properproperty-admin-page',
+  templateUrl: './admin.page.html',
+  styleUrls: ['./admin.page.css'],
+})
+export class AdminPage implements OnInit {
+
+  constructor(){
+    console.log("Constructor - Yo");
+  }
+  
+  ngOnInit() {
+    console.log("Yo");
+  }
+}
