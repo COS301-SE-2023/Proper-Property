300fc651b47de3d5543221c1d309cce6f8d58088
Rob-0ff <robofficer26@gmail.com>
2023-07-20 11:20:58 +0200

    Added footer component
    Co-authored-by: Wian Koekemoer <slaaiblaar@users.noreply.github.com>

--- libs/app/listing/feature/src/listing.module.ts
+++ libs/app/listing/feature/src/listing.module.ts
@@ -8,6 +8,7 @@ import { ListingPageRoutingModule } from './listing.routing';
 
 import { ListingPage } from './listing.page';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
 @NgModule({
   imports: [
     CommonModule,
