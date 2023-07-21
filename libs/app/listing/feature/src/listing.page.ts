f8870a67ad57944d904f4b86125d1ac7affd6930
Thapelo64 <68476683+Thapelo64@users.noreply.github.com>
2023-07-21 17:42:51 +0200

    Revert "Revert "Navbar update""
    This reverts commit 359232e73cec6418c11c21a0a2f6d342c92dce11.

--- libs/app/listing/feature/src/listing.page.ts
+++ libs/app/listing/feature/src/listing.page.ts
@@ -36,16 +36,15 @@ export class ListingPage implements OnInit{
      private listingServices : ListingsService, 
      private userServices : UserService,
      public gmapsService: GmapsService) {
+  
+
+
     this.loanAmount = 0;
     this.interestRate = 0;
     this.loanTerm = 0;
     this.monthlyPayment = 0;
     this.totalOnceOffCosts = 0;
     this.minGrossMonthlyIncome = 0;
-
-    this.user$.subscribe((user: User | null) => {
-      this.currentUser =  user;
-    });
    }
 
 
@@ -155,6 +154,8 @@ export class ListingPage implements OnInit{
     // this.user$.subscribe((user: User | null) => {
     //   this.currentUser =  user;
     // });
+
+
 
     console.log("hellow");
   }
