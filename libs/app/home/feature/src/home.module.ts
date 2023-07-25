24d584e03a5ce2021b0a4706e92f3b605687b86d
Murray Gibbon <u20440716@tuks.co.za>
2023-07-25 13:10:41 +0200

    Added footer to all pages

--- libs/app/home/feature/src/home.module.ts
+++ libs/app/home/feature/src/home.module.ts
@@ -8,14 +8,15 @@ import { HomeRoutingModule } from './home.routing';
 
 import { HomePage } from './home.page';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { FooterComponent } from '../../../footer/feature/src/footer.component';
+import { FooterModule } from '@properproperty/app/footer/feature';
 
 @NgModule({
   imports: [
     CommonModule, 
     FormsModule, 
     IonicModule, 
-    HomeRoutingModule
+    HomeRoutingModule,
+    FooterModule
   ],
   declarations: [HomePage],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
