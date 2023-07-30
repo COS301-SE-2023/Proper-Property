83fbe2c9c6125e9b65ab73276c3c33d2c9e57f26
Thapelo64 <68476683+Thapelo64@users.noreply.github.com>
2023-07-30 12:12:59 +0200

    Update create-listing.page.ts

--- libs/app/create-listing/feature/src/create-listing.page.ts
+++ libs/app/create-listing/feature/src/create-listing.page.ts
@@ -384,3 +384,162 @@ handleAddressChange(address: string): void {
   }
 
 }
+
+
+async function calculateQualityScore(photos: string[],address:string,price:string,bedrooms:string,bathrooms:string,parking:string,floor_size:string,erf_size:string,pos_type:string,env_type:string,prop_type:string,furnish_type:string,orientation:string,gmapsService: GmapsService): Promise<number>{
+            
+  let score = 0;
+
+  for(let i = 0; i < min(8, photos.length); i++){
+      score+= calculatePhotoScore(photos[i]);
+  }
+
+  if(isNumericInput(price)){
+      score+= 5;
+  } else score-=20;
+
+  if(isNumericInput(bedrooms)){
+      score+= 5;
+  } else score-=20;
+
+  if(isNumericInput(bathrooms)){
+      score+= 5;
+  } else score-=20;
+
+  if(isNumericInput(parking)){
+      score+= 5;
+  } else score-=20;
+
+  if(isNonEmptyStringInput(floor_size)){
+      score+= 5;
+  } else score-=15;
+
+  if(isNonEmptyStringInput(erf_size)){
+      score+= 5;
+  } else score-=15;
+
+  if(isNonEmptyStringInput(pos_type)){
+      score+= 5;
+  } else score-=15;
+
+  if(isNonEmptyStringInput(env_type)){
+      score+= 5;
+  } else score-=15;
+
+  if(isNonEmptyStringInput(prop_type)){
+      score+= 5;
+  } else score-=15;
+
+  if(isNonEmptyStringInput(furnish_type)){
+      score+= 5;
+  } else score-=15;
+
+  if(isNonEmptyStringInput(orientation)){
+      score+= 5;
+  } else score-=15;
+
+
+  const isGeocodable = await checkGeocodableAddress(gmapsService,address);
+
+  if (!isGeocodable) {
+    return 0;
+  }
+
+  return score;
+}
+
+function calculatePhotoScore(photo:string):number{
+
+let rating = 0;
+
+getImageDimensions(convertBlobUrlToNormalUrl(photo))
+.then(({ width, height }) => {
+  rating = 5 * (min(width, height) / max(width, height));
+  return rating;
+})
+.catch((error) => {
+  console.error(error.message);
+});
+
+// getImageDimensions( this.convertBlobUrlToNormalUrl(photo));  
+
+return -1;
+}
+
+function min(first:number,second:number):number{
+
+const ret = first < second ? first : second;
+return ret;
+}
+
+function max(first:number,second:number){
+return first > second ? first : second;
+}
+
+function convertBlobUrlToNormalUrl(blobUrl: string): string {
+const img = new Image();
+img.src = blobUrl;
+const canvas = document.createElement("canvas");
+const ctx = canvas.getContext("2d");
+if (!ctx) {
+throw new Error("Canvas context is not available.");
+}
+ctx.drawImage(img, 0, 0);
+URL.revokeObjectURL(blobUrl); // Revoke the blob URL
+return canvas.toDataURL(); // Convert to a regular data URL
+}
+
+//   getImageDimensions(imageUrl: string): void {
+//     const image = new Image();
+//     image.src = imageUrl;
+
+//     image.onload = () => {
+//       const width = image.naturalWidth;
+//       const height = image.naturalHeight;
+
+//       console.log(`Image dimensions: ${width} x ${height} pixels`);
+//     };
+//   }
+
+async function checkGeocodableAddress(gmapsService: GmapsService,address: string): Promise<boolean> {
+
+try {
+
+const geocoderResult = await gmapsService.geocodeAddress(address);
+// If the address is geocodable, the geocoderResult will not be null
+return geocoderResult !== null;
+} catch (error) {
+console.error(error);
+return false;
+}
+}
+
+
+function getImageDimensions(imageUrl: string): Promise<{ width: number; height: number }> {
+return new Promise((resolve, reject) => {
+const image = new Image();
+image.src = imageUrl;
+
+image.onload = () => {
+const width = image.naturalWidth;
+const height = image.naturalHeight;
+
+resolve({ width, height });
+};
+
+image.onerror = () => {
+reject(new Error("Failed to load the image."));
+};
+});
+}
+
+function isNumericInput(input: string): boolean {
+// Regular expression to check if the input contains only numeric characters
+const numericRegex = /^[0-9,]+$/;
+return numericRegex.test(input);
+}
+
+function isNonEmptyStringInput(input: string): boolean {
+return input.trim() !== "";
+}
+
