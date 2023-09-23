6812cd2ff724854c9a0f48cfebf86798f99def6c
Thapelo64 <68476683+Thapelo64@users.noreply.github.com>
2023-09-23 21:59:20 +0200

    Mobile notifications page

--- libs/app/alerts/feature/src/alerts.page.ts
+++ libs/app/alerts/feature/src/alerts.page.ts
@@ -1,16 +1,122 @@
-import { Component, OnInit } from '@angular/core';
+import { 
+  Component, 
+  OnDestroy, 
+  OnInit,
+  Inject,
+  HostListener, 
+  inject,
+  isDevMode
+} from '@angular/core';
+import { Store, Select } from '@ngxs/store';
+import { SubscribeToAuthState } from '@properproperty/app/auth/util';
+import { NotificationsState } from '@properproperty/app/notifications/data-access';
+import { Observable } from 'rxjs';
+import { Unsubscribe } from 'firebase/auth';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { UserProfileState,UserProfileService } from '@properproperty/app/profile/data-access';
+import { Functions } from '@angular/fire/functions';
+import { NotificationsService } from 'libs/app/notifications/data-access/src/notifications.service';
+
+
+import { DOCUMENT } from '@angular/common';
+import { Notification } from '@properproperty/api/notifications/util';
+import { UserProfile } from '@properproperty/api/profile/util';
+
+declare const gtag: any;
 
 @Component({
   selector: 'app-alerts',
   templateUrl: './alerts.page.html',
   styleUrls: ['./alerts.page.scss'],
 })
-export class AlertsPage implements OnInit {
+export class AlertsPage implements OnInit, OnDestroy {
+  
+  isMobile: boolean;
+  @Select(UserProfileState.userProfile) userProfile$!: Observable<UserProfile | null>;
+  @Select(UserProfileState.userProfileListener) userProfileListener$!: Observable<Unsubscribe | null>;
+  @Select(NotificationsState.notifications) notifications$!: Observable<Notification[] | null>;
+  @Select(NotificationsState.notificationsListener) notificationsListener$!: Observable<Unsubscribe | null>;
+  public loggedIn = false;
+  public admin = false;
+  public dev: boolean;
+  private userProfile: UserProfile | null = null;
+  private userProfileListener: Unsubscribe | null = null;
+  private notificationsListener: Unsubscribe | null = null;
+  public notifications: Notification[] = [];
+  public showNotifications = false;
+  private NotificationToken = 'whups';
+  private activatedRoute = inject(ActivatedRoute);
+  constructor(
+    private readonly router: Router, 
+    private readonly store: Store, 
+    private readonly functions: Functions, 
+    @Inject(DOCUMENT) private document: Document,
+    private profileServices : UserProfileService,
+    private readonly notificationsService: NotificationsService
+  ) {
+    this.dev = isDevMode();
+    this.loggedIn = this.userProfile$ != null && this.userProfile$ != undefined;
 
-  constructor() { }
+    this.userProfile$.subscribe((user) => {
+      this.userProfile = user;
+      this.loggedIn = user != null && user != undefined;
+      
+    });
+    // Update listener whenever is changes such that it can be unsubscribed from
+    // when the window is unloaded
+    this.userProfileListener$.subscribe((listener) => {
+      this.userProfileListener = listener;
+    });
 
+    this.notificationsListener$.subscribe((listener) => {
+      this.notificationsListener = listener;
+    });
+
+    this.notifications$.subscribe((notifications) => {
+      console.log("notifications", notifications);
+      this.notifications = notifications ?? [];
+    });
+    this.userProfile$.subscribe((profile) => {
+      this.userProfile = profile;
+      this.loggedIn = this.userProfile != null && this.userProfile != undefined;
+      this.admin = (profile && profile.admin) ?? false;
+    });
+
+    this.isMobile = window.innerWidth <= 576;
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        gtag('event', 'page_view', {
+          'page_path': event.urlAfterRedirects,
+          'page_location': this.document.location.href,
+          'is_mobile': this.isMobile ? 'yes' : 'no',
+        });
+      }
+    });
+      
+  }
+  
   ngOnInit() {
-    console.log ("Linter: Lifecycle methods should not be empty");
+    this.store.dispatch(new SubscribeToAuthState());
+  }
+  // Unsubscribes from snapshot listeners when window is unloaded
+  @HostListener('window:beforeunload')
+  ngOnDestroy() {
+    if (this.userProfileListener) {
+      this.userProfileListener();
+    }
+
+    if (this.notificationsListener) {
+      this.notificationsListener();
+    }
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    console.log(event);
+    this.isMobile = window.innerWidth <= 576;
+  }
+
+  async toggleNotifications() {
+    this.showNotifications = !this.showNotifications;
+  }
 }
