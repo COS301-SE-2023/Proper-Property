c932f27fa4976d23ed64da50e3b15ba33e71e51b
Keabetswe Madumo <98401898+MadKea@users.noreply.github.com>
2023-09-27 00:08:55 +0200

    Update app.login.spec.ts

--- apps/app/src/app/_TESTS_/app.login.spec.ts
+++ apps/app/src/app/_TESTS_/app.login.spec.ts
@@ -1,21 +1,35 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
 import { LoginPage } from '@properproperty/app/login/feature';
 
-describe('LoginPageComponent', () => {
+describe('LoginPage', () => {
   let component: LoginPage;
   let fixture: ComponentFixture<LoginPage>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [LoginPage],
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LoginPage ],
+      imports: [ IonicModule.forRoot() ]  // Add necessary imports
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginPage);
     component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // Add more unit tests for specific functionality of the LoginPage
+
+  it('should have a title', () => {
+    const expectedTitle = 'Login Page'; // Adjust this based on your actual page title
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('ion-title').textContent).toContain(expectedTitle);
+  });
+
+  // Add more unit tests based on your component behavior
+
 });
