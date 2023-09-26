25701bef8e5742e90099456a2f61f6713d50c647
Keabetswe Madumo <98401898+MadKea@users.noreply.github.com>
2023-09-27 00:01:04 +0200

    :\

---
+++ apps/app/src/app/_TESTS_/app.login.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { LoginPage } from '@properproperty/app/login/feature';
+
+describe('LoginPageComponent', () => {
+  let component: LoginPage;
+  let fixture: ComponentFixture<LoginPage>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [LoginPage],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(LoginPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
