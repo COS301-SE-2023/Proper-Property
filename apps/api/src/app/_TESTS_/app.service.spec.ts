d60877c105337a53a739bd527567102468093555
Keabetswe Madumo <98401898+MadKea@users.noreply.github.com>
2023-09-26 22:34:38 +0200

    Update app.service.spec.ts

--- apps/api/src/app/_TESTS_/app.service.spec.ts
+++ apps/api/src/app/_TESTS_/app.service.spec.ts
@@ -1,6 +1,6 @@
 import { Test } from '@nestjs/testing';
 
-import { AppService } from './app.service';
+import { AppService } from '../app.service';
 
 describe('AppService', () => {
   let service: AppService;
