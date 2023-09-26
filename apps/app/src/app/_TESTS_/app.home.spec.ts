fe803add2655af15c1bc6b17738ece7c23fc60bb
Keabetswe Madumo <98401898+MadKea@users.noreply.github.com>
2023-09-26 23:30:01 +0200

    :C

--- apps/app/src/app/_TESTS_/app.home.spec.ts
+++ apps/app/src/app/_TESTS_/app.home.spec.ts
@@ -17,14 +17,6 @@ describe('HomePage', () => {
     fixture = TestBed.createComponent(HomePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
-
-    const mockUserService = jasmine.createSpyObj('UserProfileService', ['getCurrentUser']);
-    const mockGmapsService = jasmine.createSpyObj('GmapsService', ['getRegionPredictions']);
-    const mockRouter = jasmine.createSpyObj('Router', ['navigate']);
-
-    component = new HomePage(mockUserService, mockGmapsService, mockRouter);
-    component.isMobile = false; // Set the mobile flag for testing if needed
-    component.searchQuery = ''; // Set an initial search query if needed
   });
 
   it('should create', () => {
