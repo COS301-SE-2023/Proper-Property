b40bcc0e93e4718c2fd5750e0822e4e8c470c229
Keabetswe Madumo <98401898+MadKea@users.noreply.github.com>
2023-09-26 22:24:09 +0200

    attempting different file structure for ease

--- apps/app/src/app/app.home.spec.ts
+++ apps/app/src/app/app.home.spec.ts
@@ -11,7 +11,7 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it('trying HOME', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
